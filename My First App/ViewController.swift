8ec8a3613786bd24a75638662e92b99f0c815a6d
Tarek Moubarak <tarek@hellotree.co>
2018-09-22 23:53:11 +0300

    Base Commit

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textMessage: UILabel!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        if (email.text == "tarekmoubarak@icloud.com"){
+            if (password.text == "Nadakhoury7006$"){
+                textMessage.text = "Successfully Logged In!"
+            } else{
+                textMessage.text = "Wrong Password"
+            }
+        } else{
+            //This
+            textMessage.text =  "Wrong Email " + email!.text!
+            //Or This
+//            if let email = email.text {
+//                textMessage.text =  "Wrong Email " + email
+//            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //To log in the console
+        //print("hello world!")
     }
-
 
 }
 
---
+++ README.md
@@ -0,0 +1,10 @@
+First iOS Application built with Swift 4 following this Udemy course i recommend:
+https://www.udemy.com/ios-12-developer-course/
+
+
+In this app i learned the following:
+--> Add Image to View Controller
+--> Playing with labels
+--> Playing with textfields
+--> Playing with buttons
+--> Created actions between button, textfields and label
